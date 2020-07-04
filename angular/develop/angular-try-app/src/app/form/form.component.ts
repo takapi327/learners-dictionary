a28ae8e6c16663703200ef6a5442b382d3268a63
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-07-04 11:24:17 +0900

    Create FormControl

--- angular/develop/angular-try-app/src/app/form/form.component.ts
+++ angular/develop/angular-try-app/src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators, FormGroup, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -7,7 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(10)
+  ]);
+
+  mail = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+
+  pass = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8)
+  ]);
+  
+  myForm = this.builder.group({
+    name: this.name,
+    mail: this.mail,
+    pass: this.pass
+  });
+
+  constructor(private builder: FormBuilder) { }
+
+  show() {
+    console.log(this.name.value);
+    console.log(this.mail.value);
+    console.log(this.pass.value);
+    console.log(this.myForm.value);
+  }
 
   ngOnInit(): void {
   }
