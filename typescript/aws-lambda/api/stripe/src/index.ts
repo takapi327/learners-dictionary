4647c0f1b0d537661590df8b2d00d93ff2dfeed4
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-05-11 21:58:53 +0900

    Fix Slack template

--- typescript/aws-lambda/api/stripe/src/index.ts
+++ typescript/aws-lambda/api/stripe/src/index.ts
@@ -1,10 +1,60 @@
-exports.handler = (event: any, context: any, callback: any) => {
+import { WebClient } from '@slack/web-api'
+
+exports.handler = async (event: any, context: any, callback: any) => {
+
+  const jsonData = JSON.parse(event.body)
+  const web      = new WebClient(process.env.SLACK_API_TOKEN)
+
+  const requestContextStage = event.requestContext
+    ? event.requestContext.stage
+    : 'test'
+
+  console.log("Stripe Event: %j", jsonData)
+
+  const eventType = jsonData.type ? jsonData.type : ''
   const response = {
     statusCode: 200,
-    headers: {
-      'Content-Type': 'text/html; charset=utf-8',
-    },
-    body: '<p>Hello world!</p>',
+    body: JSON.stringify({
+      message: 'Stripe webhook incoming!',
+      stage: requestContextStage,
+    }),
   }
-  callback(null, response)
+  console.log("Event Type: %j", eventType)
+
+  const params: any = {
+    channel: process.env.SLACK_CHANNEL!,
+    text:    'stripe webhook',
+    blocks:  [{
+      "type": "section",
+      "fields": [
+        {
+          "type": "mrkdwn",
+          "text": "Stripe event type"
+        },
+        {
+          "type": "mrkdwn",
+          "text": eventType
+        },
+        {
+          "type": "mrkdwn",
+          "text": "Stripe object type"
+        },
+        {
+          "type": "mrkdwn",
+          "text": jsonData['data']['object']['object']
+        },
+        {
+          "type": "mrkdwn",
+          "text": "Stripe object id"
+        },
+        {
+          "type": "mrkdwn",
+          "text": jsonData['data']['object']['id']
+        },
+      ]
+    }]
+  }
+
+  await web.chat.postMessage(params).catch(console.error)
+  await callback(null, response)
 }
